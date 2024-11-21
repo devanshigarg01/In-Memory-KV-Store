6b7cd29553940971557e241bce9f135e49d24f53
devanshigarg01 <g.devanshi@iitg.ac.in>
2024-11-21 20:19:58 +0530

    test

--- src/Server.cpp
+++ src/Server.cpp
@@ -67,7 +67,11 @@ string RespParser(istream &input) {
         {
             output = "$"+to_string(data[0].length()) +"\r\n" + data[0]+"\r\n";
         }
-    }
+     }
+     else if (command = "ping")
+     {
+      output = "+PONG\r\n";
+     }
     
     return output;
     
